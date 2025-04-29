c2f23bb3e8b48ee27007a49da78aeb20d3ec5570
tuanle277 <letuanminh2707@gmail.com>
2025-04-29 18:49:08 -0500

    chore: remove logging real

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("phishing_simulator.log"),
         logging.StreamHandler()
     ]
 )
